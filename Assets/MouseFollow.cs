c78c164c29c61f2b6f07dd7d46db0be47c13e92f
sashawikhyperfactory <sashawikhyperfactory@gmail.com>
2020-11-14 10:49:46 +0200

    minor

--- Assets/MouseFollow.cs
+++ Assets/MouseFollow.cs
@@ -2,13 +2,9 @@
 
 public class MouseFollow : MonoBehaviour
 {
+    private GameObject player;
     private void Start()
     {
-    }
-
-    private void Update()
-    {
-        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(point.x, transform.position.y, point.z);
+        player = transform.parent.gameObject;
     }
 }
--- Assets/PlayerControlls.cs
+++ Assets/PlayerControlls.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class PlayerControlls : MonoBehaviour
+public class PlayerControlls : MonoBehaviourPunCallbacks
 {
+    private PhotonView photonView;
     // Start is called before the first frame update
     void Start()
     {
-        
+        photonView = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (photonView.IsMine)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Time.deltaTime * 5 * Vector3.up);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Time.deltaTime * 5 * Vector3.left);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Time.deltaTime * 5 * Vector3.right);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Time.deltaTime * 5 * Vector3.down);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.gameObject.name);
     }
 }
